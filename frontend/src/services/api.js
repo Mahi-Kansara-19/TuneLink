4bdb18a0643453b85ab9eddd1f70256b4900d8ad
Mahi Kansara-19 /202512078_lab1.md <mahi.kansara1904@gmail.com>
2026-06-17 17:54:04 +0530

    edited api.js frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,20 +1,17 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-API.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
+API.interceptors.request.use((req) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
   }
-);
+
+  return req;
+});
 
 export default API;
